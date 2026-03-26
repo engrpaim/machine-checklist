c511a182dc62fb389d0342f98698c553bc7c4501
engrpaim <jbellosillo.137847@gmail.com>
2026-03-26 22:02:32 +0800

    automate

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "======================="
+echo "Start Update Machining Checklist"
+
+git pull origin main
+
+echo "Clear Laravel caches..."
+
+php artisan view:clear
+php artisan route:clear
+php artisan config:clear
+php artisan cache:clear
+php artisan optimized:clear
+
+
+echo "Optimize Laravel"
+php artisan optimize
+
+echo "Optimize js/pages"
+npm run build
+
+echo "Update Completed!"
